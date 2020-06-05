93e37fa38ba4f2e0338f716b8ccf3e6059debde3
Robert Alestra <bobbyalestra135@gmail.com>
2020-06-04 21:59:58 -0400

    xhr api

--- javascript/javascript/2020-06/frontend/xhrapi.js
+++ javascript/javascript/2020-06/frontend/xhrapi.js
@@ -22,8 +22,20 @@ document.addEventListener('DOMContentLoaded', function (){
 
 
         // step number 2
+        // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open
 
-        
+        // The XMLHttpRequest method open() initializes a newly-created request, or re-initializes an existing one.
+        // XMLHttpRequest.open(method, url[, async[, user[, password]]])
+        // method  The HTTP request method to use, such as "GET", "POST", "PUT", "DELETE", etc. Ignored for non-HTTP(S) URLs.
+        // url A DOMString representing the URL to send the request to.
+        // A SYNC An optional Boolean parameter, defaulting to true, indicating whether or not to perform the operation asynchronously. If this value is false, the send() method does not return until the response is received. If true, notification of a completed transaction is provided using event listeners. This must be true if the multipart attribute is true, or an exception will be thrown.
+
+
+
+
+        xhr.open('GET', 'fakeurldata.txt')
+
+
 
 
     }
