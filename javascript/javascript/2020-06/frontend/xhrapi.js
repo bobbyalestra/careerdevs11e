c642987bcabb6b90e0fc85085ec15a112fd9c5e7
Robert Alestra <bobbyalestra135@gmail.com>
2020-06-04 21:38:59 -0400

    xhr api

--- javascript/javascript/2020-06/frontend/xhrapi.js
+++ javascript/javascript/2020-06/frontend/xhrapi.js
@@ -8,14 +8,24 @@ document.addEventListener('DOMContentLoaded', function (){
         // alert(' i am a genie in a bottle why dont you Click me the right way')
     
         
+      
+        // 1 step number 1 in XHR API Process
         // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-        // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
-        
         const xhr = new XMLHttpRequest();
 
 
+
+
+        // troubleshooting
         console.log(xhr.readyState) // should == 4
-        console.log(xhr.status) // should be in 200 for successful transfer
+        console.log(xhr.status) // should be in 200-299 for successful transfer
+
+
+        // step number 2
+
+        
+
+
     }
 
 
