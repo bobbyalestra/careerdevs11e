dcdc36784e1b1d22e73daebb6ae5f36bb0abb0fa
Robert Alestra <bobbyalestra135@gmail.com>
2020-06-04 21:11:35 -0400

    xhr api

--- javascript/javascript/2020-06/frontend/xhrapi.js
+++ javascript/javascript/2020-06/frontend/xhrapi.js
@@ -1,9 +1,20 @@
+
+
 document.addEventListener('DOMContentLoaded', function (){
 
     document.getElementById('button').addEventListener("click", loadText)
     
     function loadText(){
-        alert(' i am a genie in a bottle why dont you Click me the right way')
+        // alert(' i am a genie in a bottle why dont you Click me the right way')
+        console.log("made it")
+        
+        // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+        // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+        
+        const xhr = new XMLHttpRequest();
+
+
+        console.log(xhr)
     }
 
 
