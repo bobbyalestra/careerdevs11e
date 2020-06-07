83f35050608b972b4e5c618e06babf3692f4a89f
Robert Alestra <bobbyalestra135@gmail.com>
2020-06-07 19:32:16 -0400

    rewriting the API in all JS

---
+++ javascript/javascript/2020-06/frontend/dexcomapi.js
@@ -0,0 +1,48 @@
+
+
+
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("GET", "https://api.dexcom.com/v2/users/self/devices?startDate=2017-06-16T08:00:00&endDate=2017-06-17T08:00:00");
+xhr.setRequestHeader("authorization", "Bearer {your_access_token}");
+
+xhr.send(data);
+
+var data = null;
+
+​
+
+var xhr = new XMLHttpRequest();
+
+xhr.withCredentials = true;
+
+​
+
+xhr.addEventListener("readystatechange", function () {
+
+  if (this.readyState === 4) {
+
+    console.log(this.responseText);
+
+  }
+
+});
+
+​
+
+xhr.open("GET", "https://api.dexcom.com/v2/users/self/devices?startDate=2017-06-16T08:00:00&endDate=2017-06-17T08:00:00");
+
+xhr.setRequestHeader("authorization", "Bearer {your_access_token}");
+
+​
+xhr.send(data);
+ 
---
+++ javascript/javascript/2020-06/frontend/xhrapi2.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
+
+
+
+
+let div1 = document.createElement('div');
+div1.id = 'outputData';
+document.body.appendChild(div1)
+
+
+let button1 = document.createElement('button');
+button1.id = 'dataButton';
+button1.innerText = 'Click Me For Data'
+document.body.appendChild(button1)
+
+
+document.getElementById('dataButton').addEventListener('click', loadedContent)
+
+function loadedContent(){
+
+    const xhr = new XMLHttpRequest()
+
+
+    xhr.open('GET', 'https://api.github.com/users', true)
+
+
+
+    xhr.onload= function(){
+        const responseJSON =JSON.parse(this.responseText)
+
+        if (this.readyState === 4 && this.status ===200){
+
+            html = `<ul><li>${responseJSON[i].login} ${responseJSON[1].login}   </li></ul>`
+
+            document.getElementById('outputData').innerHTML = html;
+            
+            console.log(responseJSON)
+
+        }else if (this.status ===400){
+            document.getElementById('outputData').innerText = " 404 Error Page Not Found"
+
+        }
+    
+
+    }
+    xhr.send()
+
+
+    
+}
+
+
+})
