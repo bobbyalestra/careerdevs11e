d40923bd40e9364b86ec069f8636fc90d34b3d9a
Robert Alestra <bobbyalestra135@gmail.com>
2020-06-07 18:39:53 -0400

    working on the xhr API trying to lop login names

--- javascript/javascript/2020-06/frontend/xhrapi01.js
+++ javascript/javascript/2020-06/frontend/xhrapi01.js
@@ -35,30 +35,51 @@ function loadContent(){
     // grab the variable and use the cmd GET in caps , then the server you want and make it true 
     xhr.open('GET', 'https://api.github.com/users', true)
   
-
+     
     xhr.onload = function(){
 
 
         const responseJSON =JSON.parse(this.responseText)
 
-     
+
+
+        for (let i = 0 ; i < responseJSON.user; i++){
+
+        
+
+        
+
+
+
+            // var json = {"line":[{"type":"bank","name":"ABN","account":"NL47ABNA0442660960","description":"Bijgewerkt t\/m 30-10-2014","balance":"6.266,55","image":""},{"type":"bank","name":"Rabo","account":"NL89RABO0177896647","description":"","balance":"0,00","image":""}],"total":"6.266,55"}; 
+            // for(var i = 0; i < json.line.length; i++)
+            // {
+            //     console.log("Type: " + json.line[i].type + " Name: " + json.line[i].name + " Account: " + json.line[i].account + " Description: " + json.line[i].description + " Balance: " + json.line[i].balance + " Image: " + json.line[i].image); 
+            // }
+
+
+
+        //    responseJSON.login.forEach( responseJSONUsers =>{
+        //        console.log(responseJSONUsers)
+        //    } )
               
 
         if (this.readyState === 4 && this.status === 200){
             
 
-            html = `<ul><li>${responseJSON[0].login} ${responseJSON[1].login}   </li></ul>`
+            html = `<ul><li>${responseJSON[i].login} ${responseJSON[1].login}   </li></ul>`
 
             document.getElementById('outputData').innerHTML = html;
             
-         
+            console.log(responseJSON)
 
             
         }else if 
             (this.status === 400){
                 document.getElementById(outputData).innerText = " 404 Error Page Not Found"
             }
-        
+        }
+            
     }
         xhr.send();
 
