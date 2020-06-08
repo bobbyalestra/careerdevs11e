5c6f50d62056aa46ee9d35000b6468d3d06eed06
Robert Alestra <bobbyalestra135@gmail.com>
2020-06-07 21:17:20 -0400

    working on making a program to be able to process different formula calculations

--- javascript/javascript/2020-06/frontend/sideprojects/formulas.js
+++ javascript/javascript/2020-06/frontend/sideprojects/formulas.js
@@ -13,5 +13,21 @@ document.addEventListener('DOMContentLoaded', function(){
     let h1Tag = document.createElement('h1')
     h1Tag.innerText = ' Hello World !'
     divOne.appendChild(h1Tag)
+
+    let dropDown = document.createElement('select');
+    dropDown.id = 'dropDownList'
+    dropDown.innerText='Drop Down Here'
+    h1Tag.appendChild(dropDown)
+
+
+    let numbersList = [[1,2,3],[,14,15,16], [27,28,29] ]
+
+    for (let i = 0; i < numbersList.length; i++){
+        for(let j = 0; j < numbersList[i].length; j++)
+
+        dropDown.innerHTML = `${numbersList[i][j]}`
+    }
+
+
 })
 
