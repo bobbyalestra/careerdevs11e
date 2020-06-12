14cad806088322e58c3121f0fb8d4af721d16fdf
Robert Alestra <bobbyalestra135@gmail.com>
2020-06-11 21:16:37 -0400

    finally have my weatherApp working with automatically getting your currtent location

---
+++ javascript/javascript/2020-06/frontend/openweatherapp.org/weatherapi3.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function(){
+    //making a div
+    let page = document.createElement('div');
+    page.id = 'weatherInformation';
+    //page.innerText = 'This is the Page Div';
+    document.body.appendChild(page);
+    
+    
+    
+    
+    // making a button
+    let localWeatherButton = document.createElement('button');
+    localWeatherButton.id = 'geolocation';
+    localWeatherButton.innerText = 'Click Here To See Local Weather';
+    page.appendChild(localWeatherButton);
+
+    
+    document.getElementById('geolocation').addEventListener('click', geolocate);
+
+    function geolocate() {
+        if (window.navigator && window.navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(onGeolocateSuccess);
+        }
+    
+
+
+    function onGeolocateSuccess (coordinates){
+            const {latitude, longitude} = coordinates.coords;
+            console.log(`Found coordinates: ${latitude}, ${longitude}`)
+            const url = "http://api.openweathermap.org/data/2.5/weather?";
+            const lateq = "lat=";
+            let latitudeURL = latitude;
+            const longeq = "&lon="
+            let longitudeURL = longitude;
+            const appid ='&appid=';
+            const apiKey = '303ec9718ea692dafe80b2c057c74bf9';
+
+            let combinedURL = url+lateq+latitudeURL+longeq+longitudeURL+appid+apiKey;
+
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', combinedURL, true);
+            xhr.onload = function () {
+                if (this.readyState === 4 && this.status === 200){
+                    console.log(this.responseText);
+                    let jsonText = JSON.parse(this.responseText);
+                    console.log(jsonText);
+                    let degreesF = (((jsonText.main.temp)-273.15)*(9/5)+32).toFixed(2)
+                    document.getElementById("weatherInformation").innerHTML = `${jsonText.name} is currently ${degreesF} F.`;
+
+                }else if (this.status === 404) {
+                    console.log('404 ERROR')
+                }
+
+                
+            }
+            xhr.send();
+        }
+        }
+
+        function onGeolocateError(error) {
+            console.warn(error.code, error.message);
+            
+            if (error.code === 1 ){
+                // no available location
+            } else if (error.code === 2){
+             
+            } else if (error.code === 3 );
+        
+        }})
