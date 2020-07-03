01a503b797e57a1d64bc3ae9dcc2f79b9eecfeb7
Robert Alestra <bobbyalestra135@gmail.com>
2020-07-03 18:11:30 -0400

    first react app

---
+++ sideprojects/moz-todo-react/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const subject = 'React';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         <code> Hello, {subject}! </code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
