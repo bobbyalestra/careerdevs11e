b39029e0c6268c32d1411df4801317503cf3abc9
Robert Alestra <bobbyalestra135@gmail.com>
2020-07-09 17:24:09 -0400

    diabetes app

---
+++ sideprojects/diabetesapp/diabetes01.js
@@ -0,0 +1,54 @@
+
+
+
+document.addEventListener('DOMContentLoaded',()  => {
+
+
+    let diabetesDisplay = {
+        foodDisplay: [],
+
+        displayFoodItem: function () {
+            if (this.foodDisplay.length === 0 ) {
+                alert("Please Enter An Item");
+
+            }else {
+                console.log("Food Item : ");
+
+            }
+
+            for (let i = 0; i < this.foodDisplay.length; i++){
+                console.log(this.display[i].displayText);
+                if(this.display[i].completed === true){
+                    console.log(( ' (x) ' ),this.display[i].displayText);
+                }else{
+                    console.log( '(  )',this.display[i].displayText );
+                }
+            }
+       },
+
+
+        addCurrentBg: function (displayText) {
+            this.foodDisplay.push(displayText)
+
+        },
+        
+        deleteInfo: function (position) {
+            this.foodDisplay.splice(position);
+            this.displayInfo();
+        },
+            
+        handlers: function () {
+            addCurrentBg: function () {
+                let currentBgText = document.getElementById
+            }
+
+        }
+        
+
+    }
+
+
+
+
+
+})
