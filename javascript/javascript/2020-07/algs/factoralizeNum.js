3f348d64b1ad7a0dc9feb19e88f4019751ed214f
Robert Alestra <bobbyalestra135@gmail.com>
2020-08-24 15:58:08 -0400

    factoralizing an Nth number with notes

---
+++ javascript/javascript/2020-07/algs/factoralizeNum.js
@@ -0,0 +1,21 @@
+
+
+// create a function wth a call back to set how many times you want it to run
+// set the initializer to a 'var' product equal to 1
+// make the condition the call back > than zero to find an Nth number
+// the post expression should decrement the number call back by 1 so it can meet the condition
+// well the condition is met i want to take the product to stay at 1 so the number callback will mulitply itself times itself until the count is down to zero
+// log the outcome
+// and call the function
+
+
+
+function factorialize(number) {
+    for (var product = 1; number > 0; number--) {
+      product *= number;
+    }
+
+    console.log(product)
+  }
+  
+  factorialize(0);
