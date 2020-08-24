a12b5e90c62dfb2b55f926323c199134d55bf0b3
Robert Alestra <bobbyalestra135@gmail.com>
2020-08-23 21:55:07 -0400

    reverse a string

---
+++ javascript/javascript/2020-07/algs/reversedtring.js
@@ -0,0 +1,13 @@
+
+
+function reverseString(str) {
+// you have to use 'var' to declare the variable, let wont work because it wont change in a local scope, you have to keep inside the loop so that it will constantly add it to the new array as longa as the condition is met
+    for (var reversedStr = "", i = str.length - 1 ; i > - 1;  i--) {
+      reversedStr += str[i];
+    
+    }
+
+    return reversedStr;
+  
+  }
+  console.log(reverseString)
