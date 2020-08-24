0a11774b5d663b7ebd185d563a1a00b6b4314cb6
Robert Alestra <bobbyalestra135@gmail.com>
2020-08-24 16:18:36 -0400

    find th longest word in the string

---
+++ javascript/javascript/2020-07/algs/dist/longestWord.dev.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// first create the function with a call back
+// set a local variable words and have it use the split function, and have it split the string each time it encounters a space " " so it will only take the word
+//make another local variable and set it to zero to start a Maximum length
+//create a loop with an i equalling 0
+// with he condition set to the length of the word variable
+// and incremement it by 1 each time the condtion is met
+//create a seperate condition so if the index of the word variable is greater than the length of the Max Word it will make the Max Word equal to the current index length of the current word
+// log the maxWordlenfth variable
+// call the function and set the callback to a word you want to find the largest length of
+function findLongestWordLength(string) {
+  var words = str.split(' ');
+  var maxWordLength = 0;
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > maxWordLength) {
+      maxWordLength = words[i].length;
+    }
+  }
+
+  console.log(maxWordLength);
+}
+
+findLongestWordLength('try to find the longest word in the string so that Arneezy will be happy');
--- javascript/javascript/2020-07/algs/reversedtring.js
+++ javascript/javascript/2020-07/algs/reversedtring.js
@@ -1,13 +1,22 @@
 
 
-function reverseString(str) {
-// you have to use 'var' to declare the variable, let wont work because it wont change in a local scope, you have to keep inside the loop so that it will constantly add it to the new array as longa as the condition is met
-    for (var reversedStr = "", i = str.length - 1 ; i > - 1;  i--) {
-      reversedStr += str[i];
-    
-    }
 
-    return reversedStr;
-  
+// create a function with a callback
+// create a loop so it will continue to cycle through the string 
+// set the initializer to the length of the string - 1 to get the last index
+// create a condition so that the string length is equal to - 1, equal to 0 will not include the first letter of the string
+// degrement the condition by one  each time it runs the code block
+// set the reverseString callback + and = to the string index
+// you have to use 'var' to declare the variable, let wont work because it wont change in a local scope, of the for loop , you have to keep inside the loop so that it will constantly add it to the new array as longa as the condition is met
+// log the function
+// call the function and  set the call back 
+
+
+function reverseString(string) {
+
+    for (var reverseString = "", i = string.length - 1 ; i > - 1;  i--) {
+      reverseString += string[i];  
   }
   console.log(reverseString)
+}
+reverseString('hello')
