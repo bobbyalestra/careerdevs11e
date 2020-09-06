cc71ec4e9af16d658144e38393df710d0c72d21b
Robert Alestra <bobbyalestra135@gmail.com>
2020-09-06 13:31:44 -0400

    logic work with odd numbers

--- javascript/javascript/2020-09/logic/findingoddsodds.js
+++ javascript/javascript/2020-09/logic/findingoddsodds.js
@@ -1,20 +1,44 @@
 
 // console log only the numbers that have an odd number of odd numbers
+// create a loop cycle through the number 
+// make a condition if [i] % 2 != 0 =  wil make it equal odd
 
 
 
-let numbers = [337941312819039, 5602256151929824, 30225170444787, 3551910535291105, 4041593896103, 5347039149245184, 337941624752787, 67099423720619241, 6394362198407475, 3560667783239439, 4017950948196059, 6759393197130163, 6771549142615186, 201836980226124, 3556519446278575, 3566850663741161, 3528172140783618, 3537563353849766, 6331107836502898, 4508928697689785];
-
-let oddNumbersArray = []
 
 
-for (let i = 0; i < numbers.length; i++) {
+let num = 404159389610;
+let numString = num.toString();
+let numArray = [ ];
+let numSubString = '';
+
+
+for (let i = 0; i < numString.length; i++) {
+        //console.log(numString[i]);
+        if (numString[i] % 2 != 0) {
+              numSubString.substring(numString[i])  
+                //numArray.push(numString[i])
+               // console.log(numArray);
+               console.log(numSubString);
+
+        }
+   
+}
+
+
+
+// let numbers = [337941312819039, 5602256151929824, 30225170444787, 3551910535291105, 4041593896103, 5347039149245184, 337941624752787, 67099423720619241, 6394362198407475, 3560667783239439, 4017950948196059, 6759393197130163, 6771549142615186, 201836980226124, 3556519446278575, 3566850663741161, 3528172140783618, 3537563353849766, 6331107836502898, 4508928697689785];
+// let numbString = numbers.toString();
+// let oddNumbersArray = []
+
+
+// for (let i = 0; i < numbString.length; i++) {
   
 
-        console.log(numbers[i])
+//         console.log(numbString[i])
     
 
-}
+// }
 
 
 
