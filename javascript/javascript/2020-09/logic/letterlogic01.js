e3f46e6f1284da21b4582bcb8f9efdbe5a3722bf
Robert Alestra <bobbyalestra135@gmail.com>
2020-09-10 18:47:59 -0400

    reworking letter logic

---
+++ javascript/javascript/2020-09/logic/letterlogic01.js
@@ -0,0 +1,9 @@
+
+
+let letters = 'AdffsSAkmvc';
+let lowercaseLetters = letters.toLowerCase();
+let sortLetters = lowercaseLetters.split('').sort();
+
+for (let i = 0; i < sortLetters.length ; i++) {
+    console.log(sortLetters)
+}
