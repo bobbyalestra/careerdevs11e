4ebd41011e0445ef61e7121157a31a20cc43bedf
Robert Alestra <bobbyalestra135@gmail.com>
2020-09-11 21:49:12 -0400

    update repo

---
+++ javascript/javascript/2020-09/logic/bump1.js
@@ -0,0 +1,15 @@
+
+
+// create an intereger variable called number
+// get rid of each instance of the number 1
+let number = 893714737174461;
+let numberString = number.toString();
+let numberArry = "";
+
+for (let i = 0; i < numberString.length; i ++) {
+    if (numberString[i] != '1') {
+      numberArry += numberString[i]      
+    }
+
+}
+console.log(numberArry);
--- javascript/javascript/2020-09/logic/letterlogic01.js
+++ javascript/javascript/2020-09/logic/letterlogic01.js
@@ -3,7 +3,24 @@
 let letters = 'AdffsSAkmvc';
 let lowercaseLetters = letters.toLowerCase();
 let sortLetters = lowercaseLetters.split('').sort();
+let count = 1
+let letterArr = [ ];
 
 for (let i = 0; i < sortLetters.length ; i++) {
-    console.log(sortLetters)
-}
+    // cylce through the string to get each letter
+    // console.log(sortLetters);
+
+
+    // make a condition that if the first letter match the next letter it will push it to the array
+    // and if it does match add 1 to the count
+    // 
+    if (sortLetters[i] == sortLetters[i + 1]){
+        letterArr.push(sortLetters[i]);
+        count + 1;
+        console.log(letterArr, count);
+
+    // make a condition that states if the first letter doesnt match the second letter it will log the letter without a pair
+    } else if ( sortLetters[i] !== sortLetters[i + 1]) {
+            console.log(sortLetters[i])
+        }
+    }
